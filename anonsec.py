11ab90bba257e9e98f1d38baec2ce7d0f358be46
anovni <153435050+xPloits3c@users.noreply.github.com>
2024-01-15 02:34:09 +0100

    ddos_x

--- anonsec.py
+++ anonsec.py
@@ -61,7 +61,7 @@ def Main_Menu():  # in This Function Septum The Url To Make It Usable For The Fu
     try:
         while True:
             try:
-                choice = int(input(f"\n{green}[+] Un target[0] Di più[1] > {end}"))
+                choice = int(input(f"\n{red}[+] {green}Un target[0] |Di più[1] > {end}"))
             except ValueError:
                 print(f"{red}Usa solo numeri interi!{end}")
             if choice == 1:
@@ -100,7 +100,7 @@ def Main_Menu():  # in This Function Septum The Url To Make It Usable For The Fu
             use_proxy = True
             try:
                 while True:
-                    type = int(input(f"{green}[+] Scegli [0] per SOCKS4/5 o [1] per proxy > "))
+                    type = int(input(f"{red}[+] {green}Scegli [0] per SOCKS4/5 o [1] per proxy > "))
                     if type == 0:
                         socks_mode = True
                         sock_type = int(input(f"{green}[+] Scegli [0] per SOCKS4 o [1] per SOCKS5 > "))
